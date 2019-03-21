52dd57cd4f95c601e610ad2ce9993abc57edb8b6
Mark Dunne <mark.dunne54@mail.dcu.ie>
2019-03-21 17:57:53 +0000

    fixed events

--- bot.py
+++ bot.py
@@ -17,16 +17,7 @@ class MateBot(commands.Bot):
         for extension in startup_extension:
             self.load_extension(extension)
 
-        async def on_ready(self):
-            await client.change_presence(game=Game(name="with humans"))
-            print("Logged in as " + client.user.name)
 
-        async def on_message(self, message):
-            if message.server.id == "554431614042636317" and \
-                    str(message.author) == "GitHub#0000":
-                await client.send_message(message.channel, "reloading mate")
-                await reload_helper_func()
-            await client.process_commands(message)
 
 
 async def list_servers():
@@ -41,4 +32,19 @@ TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
 
 client = MateBot()
 
-client.run(TOKEN)
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name="with humans"))
+    print("Logged in as " + client.user.name)
+
+
+@client.event
+async def on_message(message):
+    if message.server.id == "554431614042636317" and \
+            str(message.author) == "GitHub#0000":
+        await client.send_message(message.channel, "reloading mate")
+        await reload_helper_func()
+    await client.process_commands(message)
+
+client.run(TOKEN, reconnect=True)
