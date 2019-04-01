1c61308a896f56d4a05eaeb0470e4d8fa81ee0c8
Mark Dunne <mark.dunne54@mail.dcu.ie>
2019-04-01 10:50:13 +0100

    see requests

--- cogs/feature_request_cog.py
+++ cogs/feature_request_cog.py
@@ -1,8 +1,9 @@
 from functools import reduce
-import mysql.connector
 from discord.ext import commands
 from cogs.base_cog import BaseCog
-from mate_config import config
+from utils.db import Database
+
+db = None
 
 
 class FeatureRequestCog(BaseCog):
@@ -11,35 +12,22 @@ class FeatureRequestCog(BaseCog):
 
     @commands.command(pass_context=True)
     async def request(self, ctx, *args):
-        connection = get_connection()
-        cursor = connection.cursor()
-        sql_query = "INSERT INTO requests (user_name, content) VALUES (%s, %s);"
-        cursor.execute(sql_query, (ctx.message.author.name, reduce(lambda x, y: x+" " + y, args)))
-        connection.commit()
-        cursor.close()
-        connection.close()
+        get_db()
+        db.make_request(ctx.message.author.name, reduce(lambda x, y: x+" " + y, args))
+
+    @commands.command()
+    async def get_requests(self):
+        get_db()
+        await  self.bot.say(db.get_requests())
 
 
-def get_connection():
-    return mysql.connector.connect(
-        host="remotemysql.com",
-        user="2OPrqL0Qb3",
-        passwd=config.get("mysql_password"),
-        database="2OPrqL0Qb3"
-    )
+def get_db():
+    global db
+    if db is None:
+        db = Database()
 
 
 def setup(bot):
-    connection = get_connection()
-    cursor = connection.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS requests(
-             id INT auto_increment,
-            user_name varchar(255) NOT NULL ,
-            content TEXT,
-            PRIMARY KEY (id)
-        );
-        """)
-    cursor.close()
-    connection.close()
+    get_db()
+    db.create_table()
     bot.add_cog(FeatureRequestCog(bot))
---
+++ utils/db.py
@@ -0,0 +1,37 @@
+import pymysql
+from mate_config import config
+
+
+class Database:
+    def __init__(self):
+        host = "remotemysql.com"
+        user = "2OPrqL0Qb3"
+        passwd = config.get("mysql_password")
+        database = "2OPrqL0Qb3"
+        self.con = pymysql.connect(host=host, user=user, passwd=passwd, \
+                                   db=database, cursorclass=pymysql.cursors.DictCursor)
+        self.cur = self.con.cursor()
+
+    def create_table(self):
+        self.cur.execute("""
+                CREATE TABLE IF NOT EXISTS requests(
+                     id INT auto_increment,
+                    user_name varchar(255) NOT NULL ,
+                    content TEXT,
+                    PRIMARY KEY (id)
+                );
+                """)
+
+    def make_request(self, user_name, content):
+        sql_query = "INSERT INTO requests (user_name, content) VALUES (%s, %s);"
+        self.cur.execute(sql_query, (user_name, content))
+        self.cur.commit()
+
+    def get_requests(self):
+        sql_query = "SELECT * FROM requests"
+        self.cur.execute(sql_query)
+        return self.cur.fetchall()
+
+    def __del__(self):
+        self.cur.close()
+        self.con.close()
